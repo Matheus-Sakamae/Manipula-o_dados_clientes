9e8112b00c7976ef65dcd530e7f173f5da28c815
Matheus-Sakamae <jujitsu_fios_6h@icloud.com>
2026-04-20 21:51:33 -0300

    Separando importação do banco com a main do projeto
    Separando importação do banco com a main do projeto

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import os
-import mysql.connector
-from dotenv import load_dotenv
+from db import get_connection
 
 def main():
-    load_dotenv()
-
-    connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-    )
+    connection = get_connection()
 
     cursor = connection.cursor()
     cursor.execute("SELECT DATABASE()")
